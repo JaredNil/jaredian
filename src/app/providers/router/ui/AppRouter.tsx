8cfe872cbac3812a418f1608193d277e6dd0faf6
JaredN <jared.bozh@gmail.com>
2024-04-07 14:09:27 +0800

    mainpage_content

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -3,7 +3,7 @@
 import { Suspense, useCallback } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import PageLoader from 'widgets/PageLoader/PageLoader';
+import { Loader } from 'shared/ui/Loader/Loader';
 
 import { useSelector } from 'react-redux';
 import { AppRoutesProps, routeConfig } from 'shared/config/routeConfig/routeConfig';
@@ -11,14 +11,8 @@ import { RequireAuth } from './RequireAuth';
 
 const AppRouter: React.FC = () => {
 	const renderWithWrapper = useCallback((route: AppRoutesProps) => {
-		const element = <Suspense fallback={<PageLoader />}>{route.element}</Suspense>;
-		return (
-			<Route
-				key={route.path}
-				path={route.path}
-				element={route.authOnly ? <RequireAuth>{element}</RequireAuth> : <>{element}</>}
-			/>
-		);
+		const element = <Suspense fallback={<Loader />}>{route.element}</Suspense>;
+		return <Route key={route.path} path={route.path} element={route.authOnly ? <RequireAuth>{element}</RequireAuth> : <>{element}</>} />;
 	}, []);
 
 	return <Routes>{Object.values(routeConfig).map(renderWithWrapper)}</Routes>;
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -12,26 +12,22 @@ const MainPage: React.FC = () => {
 	};
 
 	return (
-		<Loader />
-		// <div>
-		// 	<div>
-		// 		Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure incidunt nisi obcaecati neque sint,
-		// 		impedit nesciunt. Necessitatibus cumque veniam natus suscipit. Accusantium libero dolores quidem
-		// 		voluptate tempora harum eos natus.
-		// 	</div>
-		// 	<Title size="m">Lorem</Title>
-		// 	<div>
-		// 		Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure incidunt nisi obcaecati neque sint,
-		// 		impedit nesciunt. Necessitatibus cumque veniam natus suscipit. Accusantium libero dolores quidem
-		// 		voluptate tempora harum eos natus.
-		// 	</div>
-		// 	<Title size="m">Lorem</Title>
-		// 	<div>
-		// 		Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure incidunt nisi obcaecati neque sint,
-		// 		impedit nesciunt. Necessitatibus cumque veniam natus suscipit. Accusantium libero dolores quidem
-		// 		voluptate tempora harum eos natus.
-		// 	</div>
-		// </div>
+		<div>
+			<div>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure incidunt nisi obcaecati neque sint, impedit nesciunt. Necessitatibus cumque veniam natus
+				suscipit. Accusantium libero dolores quidem voluptate tempora harum eos natus.
+			</div>
+			<Title size="m">Lorem</Title>
+			<div>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure incidunt nisi obcaecati neque sint, impedit nesciunt. Necessitatibus cumque veniam natus
+				suscipit. Accusantium libero dolores quidem voluptate tempora harum eos natus.
+			</div>
+			<Title size="m">Lorem</Title>
+			<div>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure incidunt nisi obcaecati neque sint, impedit nesciunt. Necessitatibus cumque veniam natus
+				suscipit. Accusantium libero dolores quidem voluptate tempora harum eos natus.
+			</div>
+		</div>
 	);
 };
 
