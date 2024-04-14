36fe45076fecc71d2fedf659330f56e70ed5eadf
JaredN <jared.bozh@gmail.com>
2024-04-14 15:21:04 +0800

    fix-index

--- src/features/Burger/index.ts
+++ src/features/Burger/index.ts
@@ -1,7 +1,1 @@
-export { authAction, authReducer } from './model/slice/authSlice';
-
-export { AuthSchema } from './model/types/AuthSchema';
-
-export { getAuthUsername, getAuthError, getAuthIsLoading, getAuthPassword, getAuthIsValid } from './model/selector/AuthSelectors';
-
-export { AuthModal } from './ui/BurgerModal/BurgerModal';
+export { BurgerModal } from './ui/BurgerModal/BurgerModal';
